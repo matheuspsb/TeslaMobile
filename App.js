a69d6f63c85dc1fd55be433979656549406e8a25
Matheus <matheusslg1@gmail.com>
2022-07-21 10:22:20 -0300

    feat: button component and cardItem component done

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CarItem />
+      
       <StatusBar style="auto" />
     </View>
   );
