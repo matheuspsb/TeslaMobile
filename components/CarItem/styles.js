8356919b318f40ca65404361c5d7126f6adba5d4
Matheus <matheusslg1@gmail.com>
2022-07-21 10:46:00 -0300

    feat: carItemComponent done

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -5,30 +5,40 @@ const styles = StyleSheet.create({
     carContainer: {
         width: '100%',
         height: '100%',
-      },
+    },
     
-      titles: {
+    titles: {
         marginTop: '30%',
         width: '100%',
         alignItems: 'center',
-      },
-    
-      title: {
+    },
+
+    title: {
         fontSize: 40,
         fontWeight: '500',
-      },
-    
-      subtitle: {
+    },
+
+    subtitleCTA: {
+        textDecorationLine: 'underline',
+    },
+
+    subtitle: {
         fontSize: 16,
         color: '#5c5e62',
-      },
-    
-      image: {
+    },
+
+    image: {
         width: '100%',
         height: '100%',
         resizeMode: 'cover',
         position: 'absolute',
-      }
+    },
+
+    buttonsContainer: {
+        position: "absolute",
+        bottom: 50,
+        width: '100%',
+    }
 });
 
 export default styles;
