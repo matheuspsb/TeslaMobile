869835702279e33863d8fde64c1ee9edcd10a24f
Matheus <matheusslg1@gmail.com>
2022-07-25 09:14:32 -0300

    feat: Header and CarsList

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { View, Text, ImageBackground } from "react-native";
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
 import StyledButton from "../StyledButton";
-import styles from "./styles";
+import styles from './styles';
 
 const CarItem = (props) => {
 
-    const { name, tagline, taglineCTA, image } = props;
+  const { name, tagline, taglineCTA, image } = props.car;
 
-    return (
-        <View style={styles.carContainer}>
-        <ImageBackground 
-          source={image}
-          style={styles.image}
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={image}
+        style={styles.image}
+      />
+
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline}
+          {' '}
+          <Text style={styles.subtitleCTA}>
+            {taglineCTA}
+          </Text>
+        </Text>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Custom Order"}
+          onPress={() => {
+            console.warn("Custom Order was pressed");
+          }}
         />
 
-        <View style={styles.titles}>
-          <Text style={styles.title}>{name}</Text>
-          <Text style={styles.subtitle}>
-            {tagline}
-            {' '}
-            <Text style={styles.subtitleCTA}>
-                {taglineCTA}
-            </Text>
-        </Text>
-        </View>
-        
-        <View style={styles.buttonsContainer}>
-            <StyledButton 
-                type="primary" 
-                content={"Custom Order"} 
-                onPress={()=>{
-                    console.warn('Custom Order was pressed');
-                }}
-            />
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory was pressed");
+          }}
+        />
+      </View>
 
-            <StyledButton 
-                type="secondary" 
-                content={"Existing Inventory"} 
-                onPress={()=>{
-                    console.warn('Existing Inventory was pressed');
-                }}
-            />
-        </View>
-        
-        </View>
-    );
+    </View>
+  );
 };
 
-export default CarItem
+export default CarItem;
--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { View, Text, Pressable } from "react-native";
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
 import styles from './styles';
 
 const StyledButton = (props) => {
 
-    const { type, content, onPress } = props;
-    
-    const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
-    const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
+  const { type, content, onPress } = props;
 
-    return (
-        <View style={styles.container}>
-            <Pressable
-                style={[styles.button, {backgroundColor: backgroundColor}]}
-                onPress={()=> onPress()}
-            >
-                <Text style={[styles.text, {color: textColor}]}>{content}</Text>
-            </Pressable>
-        </View>
-    );
-}
-export default StyledButton
+  const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
+  const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
+
+  return (
+    <View style={styles.container}>
+      <Pressable
+        style={[styles.button, {backgroundColor: backgroundColor}]}
+        onPress={() => onPress()}
+      >
+        <Text style={[styles.text, {color: textColor}]}>{content}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default StyledButton;
